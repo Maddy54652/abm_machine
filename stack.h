57c9a8aba6e513ac25563c2fbf82bdb1c0d97399
Maddy <37761862+Maddy54652@users.noreply.github.com>
2023-11-06 11:34:49 -0800

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,4 +1,19 @@
 #ifndef STACK_H
 #define STACK_H
 
+void push(void);
+//insert stack element
+
+void pop(void);
+//remove element
+
+void top(void);
+//returns top of stack
+
+bool isEmpty(void);
+//returns true if stack is empty
+
+void size(void);
+//returns stack size
+
 #endif
