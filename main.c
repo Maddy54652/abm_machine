a3003d991523cc9ff6244d5b25b7492af940bb5c
Maddy <37761862+Maddy54652@users.noreply.github.com>
2023-11-17 19:56:30 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
+#include"stack.h"
+
 
 int main(int argc,char* argv[]){
 
 	//checked for file name
-	printf("%s\n",argv[1]);
+	//printf("%s\n",argv[1]);
 	
+	STACK whatever;//this is just to make sure I made the struct right
+	
+	whatever.size = 6;
+	
+	printf("%d\n",whatever.size);
 
 	return 0;
 }
