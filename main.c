cd6c49f846e91338872d8319e48df683cf3896cf
Maddy <37761862+Maddy54652@users.noreply.github.com>
2024-04-05 18:03:08 -0700

    Update main.c
    test of stack functions

--- main.c
+++ main.c
@@ -4,14 +4,30 @@
 
 int main(int argc,char* argv[]){
 
-	//checked for file name
-	//printf("%s\n",argv[1]);
+	//testing the stack functions
 	
-	STACK whatever;//this is just to make sure I made the struct right
-	
-	whatever.size = 6;
-	
-	printf("%d\n",whatever.size);
+	StackPtr Test_Stack;
+	int i;
+	Test_Stack = initStack();
+	isEmpty(Test_Stack);
+	push(Test_Stack,'a');
+	push(Test_Stack,'#');
+	push(Test_Stack,'|');
+	push(Test_Stack,'R');
+	isEmpty(Test_Stack);
+	peek(Test_Stack);
+	for(i = 0;i<4;i++)
+	{
+		peek(Test_Stack);
+		pop(Test_Stack);
+	}
+	isEmpty(Test_Stack);
+	push(Test_Stack,'$');
+	push(Test_Stack,67);
+	empty(Test_Stack);
+	isEmpty(Test_Stack);
+	destroyStack(Test_Stack);
 
+	printf("It work.\n");
 	return 0;
 }
